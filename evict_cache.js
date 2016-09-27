42df2bb576ef9958ce32b06ead809880653d751c
akshay <akshay@genius.com>
2016-09-27 15:10:37 -0400

    CR changes

--- evict_cache.js
+++ evict_cache.js
@@ -1,28 +1,36 @@
+var AMP_CDN_HOST = 'cdn.ampproject.org', GOOGLE_HOSTNAME = 'www.google.com';
+var AMP_PATH_CONTENTS_BEFORE_CACHE_URL = /.*\/c\/s\//, GOOGLE_AMP_PATH_CONTENTS_BEFORE_CACHE_URL = '/amp/';
+var AMP_CACHE_EVICTION_URL = 'https://cdn.ampproject.org/update-ping/c/s/';
+var REDIRECT_TO_URL_PREFIX = 'https://cdn.ampproject.org/c/s/';
+var SUCCESS_IMAGE_PATH = 'images/checky.png';
+var ERROR_IMAGE_PATH = 'images/checky.png';
+
+
 chrome.tabs.query(
   {
     active: true,
     currentWindow: true
-  }, function (tabs) {
-      var tab = tabs[0];
-    var currentUrl = new URL(tab.url), url_to_cache;
+  }, tabs => {
+    var tab = tabs[0];
+    var currentUrl = new URL(tab.url), urlToCache;
 
-    if (currentUrl.hostname === "cdn.ampproject.org") {
-      url_to_cache = currentUrl.pathname.replace(/.*\/c\/s\//, '');
-    } else if (currentUrl.hostname === "www.google.com" && currentUrl.pathname.startsWith("/amp/")) {
-      url_to_cache = currentUrl.pathname.replace(/\/amp\//, '');
+    if (currentUrl.hostname === AMP_CDN_HOST) {
+      urlToCache = currentUrl.pathname.replace(AMP_PATH_CONTENTS_BEFORE_CACHE_URL, '');
+    } else if (currentUrl.hostname === GOOGLE_HOSTNAME && currentUrl.pathname.startsWith(GOOGLE_AMP_PATH_CONTENTS_BEFORE_CACHE_URL)) {
+      urlToCache = currentUrl.pathname.replace(GOOGLE_AMP_PATH_CONTENTS_BEFORE_CACHE_URL, '');
     } else {
-      url_to_cache = currentUrl.hostname + currentUrl.pathname;
+      urlToCache = currentUrl.hostname + currentUrl.pathname;
     }
 
-    fetch("https://cdn.ampproject.org/update-ping/c/s/" + url_to_cache).then(function(response) {
-      if(response.status == 204) {
-        document.getElementById("status").src = "images/checky.png";
-        chrome.tabs.update(tab.id, {url: "https://www.google.com/amp/"+url_to_cache});
+    fetch(AMP_CACHE_EVICTION_URL + urlToCache).then(function(response) {
+      if(response.status === 204) {
+        document.getElementById('status').src = SUCCESS_IMAGE_PATH;
+        chrome.tabs.update(tab.id, {url: REDIRECT_TO_URL_PREFIX + urlToCache});
       } else {
-        throw response;
+        return Promise.reject(response);
       }
     }).catch(function() {
-      document.getElementById("status").src = "images/error.png";
+      document.getElementById('status').src = ERROR_IMAGE_PATH;
     });
   }
 );
